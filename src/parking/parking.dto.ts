b9975099c3e96b639dee9bf8359a7cd8db7ad888
nk-rodrigues <nkrodrigues98@gmail.com>
2022-12-06 18:44:28 +0530

    updated parking validations

--- src/parking/parking.dto.ts
+++ src/parking/parking.dto.ts
@@ -1,13 +1,15 @@
-import {isString, IsNotEmpty, IsNumber} from 'class-validator'
+import { IsNotEmpty, IsNumber, Min} from 'class-validator'
 
 export class ParkingDTO {
     @IsNumber()
     @IsNotEmpty()
+    @Min(1)
     no_of_slot: number;
 }
 
 export class ParkingExtendDTO {
     @IsNumber()
     @IsNotEmpty()
+    @Min(1)
     increment_slot: number;
 }
